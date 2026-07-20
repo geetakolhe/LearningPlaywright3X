705c54bc99c1ec25c3b5612fa576fa022c1f5d47
geeta <geeta@example.com>
2026-07-20 22:15:06 +0530

    Data Type and Operator code Practice

---
+++ 05_chapter_Operator/16_Comparison_Operator.js
@@ -0,0 +1,30 @@
+// Comparison Operators are used to compare two values and return a boolean value (true or false) based on the comparison.
+
+// =, ==, ===, !=, !==, >, <, >=, <=
+
+// = -> Assignment operator, used to assign a value to a variable
+
+// == -> Equality operator, checks if two values are equal (type coercion is performed)
+
+// === -> Strict equality operator, checks if two values are equal and of the same type (no type coercion)
+
+// != -> Inequality operator, checks if two values are not equal (type coercion is performed) 
+
+// !== -> Strict inequality operator, checks if two values are not equal or not of the same type (no type coercion)
+
+// > -> Greater than operator, checks if the left value is greater than the right value
+// < -> Less than operator, checks if the left value is less than the right value
+// >= -> Greater than or equal operator, checks if the left value is greater than or equal to the right value
+// <= -> Less than or equal operator, checks if the left value is less than or equal to the right value
+
+console.log(10 == "10"); // true
+console.log(10 === "10"); // false
+console.log(10 > 5); // true
+console.log(10 < 5); // false
+console.log(10 >= 10); // true
+console.log(10 <= 10); // true
+console.log(10 != "10"); // false
+console.log(10 !== "10"); // true
+console.log(10 != 5); // true
+console.log(10 !== 5); // true
+
